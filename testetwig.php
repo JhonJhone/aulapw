7fac4ee63527eaa0244066511bef85b9be776bf6
JhonCarli <126169393+JhonCarli@users.noreply.github.com>
2023-02-24 09:40:37 -0300

    inicial

---
+++ testetwig.php
@@ -0,0 +1,17 @@
+<?php
+
+// testetwig.php
+
+require ('vendor/autoload.php');
+
+$loader = new \Twig\Loader\FilesystemLoader('./templates');
+
+$twig = new \Twig\Environment($loader);
+ $template = $twig->load('teste.html');
+ echo $template->render([
+     'nome' => 'Jhones',
+     'idade' => 17,
+     'title' => 'testezão',
+ ]);
+
+
