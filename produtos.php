c9096e40f3cfd337ed7f830df765fc78c436fff7
JhonCarli <126169393+JhonCarli@users.noreply.github.com>
2023-02-24 10:28:01 -0300

    escape

--- produtos.php
+++ produtos.php
@@ -8,6 +8,23 @@ $loader = new \Twig\Loader\FilesystemLoader('./templates');
 $twig = new \Twig\Environment($loader);
 
 $template = $twig->load('produtos.html');
+
+$produtos = [
+    [
+        'nome' => 'Chinelo',
+        'preco' => 30,
+    ],
+    [
+        'nome' => 'Camiseta',
+        'preco' => 50,
+    ],
+    [
+        'nome' => 'Boné',
+        'preco' => 39.9,
+    ],
+];
+
 echo $template->render([
-    'title' => 'products'
+    'title' => 'products',
+    'produtos' => $produtos,
 ]);
