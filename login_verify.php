c40387313bc62d68c3b7e8cb07ba0c877ea8f2e7
JhonJhone <126169393+JhonJhone@users.noreply.github.com>
2023-03-09 10:00:50 -0300

    SENHA ZONA IRADA

--- login_verify.php
+++ login_verify.php
@@ -1,14 +1,28 @@
 <?php
 
+require('pdo.inc.php');
 $user = $_POST['user'];
 $pass = $_POST['pass'];
 
-if ($user == 'jhon' && $pass == '123') {
+//Cria a consulta e aguarda os dados
+$sql = $pdo->prepare('SELECT * FROM usuarios WHERE username = :usr AND senha = :pass');
+
+//Adiciona os dados na consulta
+$sql->bindParam(':usr', $user);
+$sql->bindParam(':pass', $pass);
+
+//Roda a consulta no banco
+$sql->execute();
+
+//Se encontrou o usuario
+if ($sql->rowCount()) {
     #Login sucesso
+
+    $user = $sql->fetch(PDO::FETCH_OBJ);
 
     #armazenar user
     session_start();
-    $_SESSION['user'] = 'jhon';
+    $_SESSION['user'] = $user->nome;
 
     #redirecionar usuario
     header('location:welcome.php');
---
+++ pdo.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+$pdo = new PDO('mysql:host=localhost; dbname=sistematwig', 'root', '');
+
+
+//*Exemplo*
+
+//$usuarios = $conec->query('SELECT * FROM usuarios');
+
+//while($user = $usuarios->fetch(PDO::FETCH_OBJ)){
+//    var_dump($user);
+//}
