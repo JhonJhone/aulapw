a905dd64f809b671acbbde46f8ed3553ef544938
JhonCarli <126169393+JhonJhone@users.noreply.github.com>
2023-03-24 11:02:56 -0300

    salva de arquivos
    69

--- recuperar_senha_escolher.php
+++ recuperar_senha_escolher.php
@@ -1,5 +1,4 @@
 <?php
-
 require('twig_carregar.php');
 require('pdo.inc.php');
 
@@ -12,26 +11,34 @@ if(!$token){
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $password = $_POST['senha'] ?? false;
+    $password_conf = $_POST['senha_confirma'] ?? false;
+
     $password = trim($password);
+    $password_conf = trim($password_conf);
     //tratamento para caso de senha falsa
     //...
+
+    if($password == $password_conf){
     
     $sql = $pdo->prepare('UPDATE usuarios SET senha = :senha, recupera_token = NULL WHERE recupera_token = :token');
     $sql->execute([
         ':senha' => password_hash($password, PASSWORD_BCRYPT),
         ':token' => $token,
     ]);
-
+    
     header('location:login.php?erro=3');
 
     die;
+}
+$msg = 'Não é possível. Como tu erra a porra da senha caralho?!';
+
 }
 
 $sql = $pdo->prepare('SELECT * FROM usuarios WHERE recupera_token = ?');
 $sql->execute([$token]);
 
 if($sql->rowCount() == 1) {
-    echo $twig->render('recuperar_senha_escolher.html', ['token' => $token]);
+    echo $twig->render('recuperar_senha_escolher.html', ['token' => $token, 'msg' => $msg ?? false]);
 }else {
     header('location:login.php');
     die;
