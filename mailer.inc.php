eddd73f26731037ad1ecfa2a93689fb4d98d338c
JhonCarli <126169393+JhonJhone@users.noreply.github.com>
2023-05-05 09:28:16 -0300

    Update mailer.inc.php

--- mailer.inc.php
+++ mailer.inc.php
@@ -8,7 +8,7 @@ $mail = new PHPMailer(true);
 $mail->isSMTP();
 $mail->Host = '';
 $mail->SMTPAuth = true;
-$mail->Username = 'juavitor1@gmail.com';
+$mail->Username = '';
 //troxão
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
 $mail->Port = 465;
